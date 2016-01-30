d369a26cecca474046f2ad149c08cd71fe63a1e1
KluevaEvgeniya <Kluevaevgeniya@gmail.com>
2016-01-30 11:01:38 +0500

    Тарифы ЖКХ

--- RentCalculator.java
+++ RentCalculator.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 +public class RentCalculator {
 
     // Тарифы ЖКХ
-
-
-
-
-
-
-
-
+    static final double Tsod = 18.69;       // Тариф содержания
+    static final double Tcap = 5.80;        // Тариф капитального ремонта
+    static final double Totop = 21.67;      // Тариф отопления
+    static final double Thvs = 18.50;       // Тариф холодного водоснабжения
+    static final double Tgvs = 123.58;      // Тариф горячего водоснабжения
+    static final double Tvod = 14.50;       // Тариф водоотведения
+    static final double Tel = 2.35;         // Тариф электроэнергии
+    static final double Tgas = 4.72;        // Тариф за газ
 
     // Данные плательщика
    
@@ -29,7 +29,7 @@ import java.util.Scanner;
 
 
 
-  
 
-    
+
+   
 }
