f35139b84bacd469fb25808c4cce5b2afe5aa04d
AlexaKarmaza <alexak09@mail.ru>
2016-01-30 10:56:16 +0500

    Структура проекта

---
+++ RentCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
++public class RentCalculator {
+
+    // Тарифы ЖКХ
+
+
+
+
+
+
+
+
+
+    // Данные плательщика
+   
+
+
+
+
+
+
+    // Итоговые суммы
+  
+
+
+
+
+
+
+
+  
+
+    
+}
